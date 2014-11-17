d44667e9511cf96e036443e19f4265b0f3a298ff
Rajesh Kumar <kommirajesh@gmail.com>
2014-11-17 14:01:41 +0530

    adding repository

---
+++ E-paani/home.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="description" content="Order for water bottle online epaani">
+<title>Order for water bottle</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+<!-- Google Webfont -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<!-- Logo & Navigation starts -->
+      
+   <div class="header">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-3">
+                  <!-- Logo -->
+                  <div class="logo">
+                     <h1><a href="index.html">e paani</a></h1>
+                  </div>
+                  
+               </div>
+               <div id="headerRightSectioBox">
+					<div id="helplineBox">
+						<div class="helplineTitle">24 x 7 Helpline</div>
+						<div class="helplineNumber">812-101-9585</div>
+					</div>
+                    <div class="headerLinks"><a href="#" data-toggle="modal" data-target="#login-modal">logout</a>&nbsp;&nbsp;|  &nbsp;&nbsp;<a href="#cancel-booking">Cancellation</a>&nbsp;&nbsp;   | &nbsp;&nbsp;  <a href="#quick-enquiry">Enquiry</a> </div>
+
+               </div>
+            </div>
+      </div>
+   </div>
+<!-- Logo & Navigation ends -->
+<!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top navfix" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          
+        </div>
+        <div class="navbar-collapse collapse" id="navigation">
+          <ul class="nav navbar-nav">
+            <li class="active"><a class="glyphicon glyphicon-home" href="index.html">&nbsp;Home</a></li>
+            <li><a class="glyphicon glyphicon-book" href="#about">&nbsp;About</a></li>
+            <li><a class="glyphicon glyphicon-phone" href="#contact">&nbsp;Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+<!-----------------------main container holder-------------------------->
+ <div class="container">
+    <div class="row content">
+        <div class="col-lg-3 col-ofset-2 content-left">
+                <h4>Categories</h4>
+                <div class="list-group categories">
+                     <a href="#" class="list-group-item">Books <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Cameras & Photo <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Cell Phones & Accessories <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Clothing, Shoes & Accessories <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Computers & Networking <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">DVDs & Movies <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Health & Beauty <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Music <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     <a href="#" class="list-group-item">Toys & Hobbies <span class="glyphicon glyphicon-chevron-right"></span></a>
+                </div>
+        </div>
+        <!--
+        <div class="container">
+
+            <div class="row">
+                
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="portfolio-item.html">
+                        <img class="img-responsive img-home-portfolio" src="http://www.coke2home.com/images/product/04443e9db76a1b10fa66d1e052a1b593.jpg">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="portfolio-item.html">
+                        <img class="img-responsive img-home-portfolio" src="http://www.coke2home.com/images/product/04b5fbae7da91bdf7f379b7022f2a43b.png">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="portfolio-item.html">
+                        <img class="img-responsive img-home-portfolio" src="http://www.coke2home.com/images/product/029e3cb10a4c86bdf3cc69718aa80b8e.jpg">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="portfolio-item.html">
+                        <img class="img-responsive img-home-portfolio" src="http://www.coke2home.com/images/product/2579a510f654fc848ea369dab4a70584.jpg">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="portfolio-item.html">
+                        <img class="img-responsive img-home-portfolio" src="http://www.coke2home.com/images/product/25443ff4d2db66d70364369ea5496534.jpg">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="portfolio-item.html">
+                        <img class="img-responsive img-home-portfolio" src="http://placehold.it/700x450">
+                    </a>
+                </div>
+            </div>
+             </div>
+         -->
+
+        <!-- portfolio 2 thumnail gallery-->
+
+       
+    <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/images.jpg" class="thumbnail img-responsive">
+           
+            <p><strong><b>Bisleri Jumbo Pack<br><br>1 ltr<br><br>
+Rs. 250</b></strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/197351_2-bisleri-mineral-water.jpg" class="thumbnail img-responsive">
+        <p><strong><b>Bisleri 1 ltr carton Pack<br><br>
+1 ltr Per Item<br><br>
+Rs. 228</b></strong></p>
+        </a>
+        
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/bis.jpg" class="thumbnail img-responsive">
+       <p><strong><b>Bisleri water bottle <br><br>
+1 ltr<br><br>
+Rs. 30</b></strong></p>
+             </a>
+    </div>
+        <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/bis.png" class="thumbnail img-responsive">
+        <p><strong>Bisleri 25 ltr can</strong></p>
+            <p><strong>25 ltr</strong></p>
+            <p><strong>Rs. 55</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://www.coke2home.com/images/product/0181ca39ae64d91cb69cab47c7dfd660.jpg" class="thumbnail img-responsive">
+             <p><strong>Sprite Pack</strong></p>
+            <p><strong>1 1/2 ltr</strong></p>
+            <p><strong>Rs. 60</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://www.coke2home.com/images/product/003f39377ed8e903b6b5cc0e042f488e.png" class="thumbnail img-responsive">
+        <p><strong>Minute Maid Pulp Orangek</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 25</strong></p>
+        </a>
+    </div>
+
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://www.coke2home.com/images/product/053c8809b114e00295ebe3a83a40141a.jpg" class="thumbnail img-responsive">
+        <p><strong>Fanta Pack</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 100</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/0691e1edb02387108fe898d6ac39de0b.jpg" class="thumbnail img-responsive">
+         <p><strong>Thums Up Pack</strong></p>
+            <p><strong> 1 ltr</strong></p>
+            <p><strong>Rs. 120</strong></p>
+        </a>
+    </div>
+            <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/0553413dcf2eacb7604e9f26ebead35c.jpg" class="thumbnail img-responsive">
+         <p><strong>coco cola bottle</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 20</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/coke1.png" class="thumbnail img-responsive">
+         <p><strong>Cock Zero</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 80</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/ef98120bce620b3d8c0e16781f40ff1e.png" class="thumbnail img-responsive">
+         <p><strong>Limca</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 3o</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/f1cdd9b04b40a4f75fb98114b2d17953.jpg" class="thumbnail img-responsive">
+         <p><strong>Fanta </strong></p>
+            <p><strong>1 ltr</strong></p>
+            <p><strong>Rs. 40</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/f08d8d70ce4344d4da8aad19b09a4315.jpg" class="thumbnail img-responsive">
+         <p><strong>Mainute Maide Pack</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 200</strong></p>
+        </a>
+    </div>
+
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/f379d39925a434692e79b23c2a49d21c.jpg" class="thumbnail img-responsive">
+         <p><strong>schweppes soda water</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 25</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/1 (2).jpg" class="thumbnail img-responsive">
+         <p><strong>schwepper Tonic water</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 50</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/2 (2).jpg" class="thumbnail img-responsive">
+         <p><strong>Sprite</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 30</strong></p>
+        </a>
+    </div>
+     <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/3 (2).jpg" class="thumbnail img-responsive">
+         <p><strong>Minute maide Pack</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 75</strong></p>
+        </a>
+    </div>
+
+        <div class="col-lg-3 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="images/4 (2).jpg" class="thumbnail img-responsive">
+         <p><strong>Burn</strong></p>
+            <p><strong>250 Ml</strong></p>
+            <p><strong>Rs. 50</strong></p>
+        </a>
+    </div>
+  </div>
+  <hr>
+  	<h2 class="text-center"><a href="#">Footer Page</a></h2>
+  <hr>
+</div>
+         
+     </div>
+ </div>    
+
+<!--main container end-->
+
+<!------------------header----------------------->
+
+<!-- Modal -->
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Login</h4>
+      </div>
+      <div class="modal-body">
+       <form class="form-inline" role="form">
+  			<div class="form-group">
+   			 <label class="sr-only" for="exampleInputEmail2">Email address</label>
+   			 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+ 		   </div>
+  		<div class="form-group">
+    		<label class="sr-only" for="exampleInputPassword2">Password</label>
+    		<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+  		</div>
+  		<div class="checkbox">
+    		<label>
+     		 <input type="checkbox"> Remember me
+    		</label>
+ 	 </div>
+		</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-dismiss="modal">Login</button>
+        <button type="button" class="btn btn-danger">Cancel</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+        $('.carousel').carousel()
+		interval: 200
+        })
+      }(window.jQuery)
+    </script>
+ <script type="text/javascript">
+$(function () {
+    $(".brand-popover img").popover({
+        placement : 'top'
+    });
+});
+</script>  
+</body>
+</html>
